6fa7beeee51189dc9d0991ffca98a9d34240f5fc
Mkanobi <maksymilian_piotrowski@wp.pl>
2022-11-21 23:03:58 +0100

    Revert "Fixed login request to JSON form"
    This reverts commit d0f00460767ea7bc1223a7a044e40044ac9e7238.

--- src/main/java/com/progzesp/stalking/security/LoginAuthenticationFilter.java
+++ src/main/java/com/progzesp/stalking/security/LoginAuthenticationFilter.java
@@ -4,7 +4,6 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -20,7 +19,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -37,14 +35,8 @@ public class LoginAuthenticationFilter extends UsernamePasswordAuthenticationFil
 
 	@Override
 	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-		String username, password;
-		try {
-			Map<String, String> requestMap = new ObjectMapper().readValue(request.getInputStream(), Map.class);
-			username = requestMap.get("login");
-			password = requestMap.get("password");
-		} catch (IOException e) {
-			throw new AuthenticationServiceException(e.getMessage(), e);
-		}
+		String username = request.getParameter("login");
+		String password = request.getParameter("password");
 		UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
 		return authenticationManager.authenticate(authenticationToken);
 	}
